251397bf567e1780d754f42fddd99d984ed70ac3
sakabar <takafumi.sakakibara@gmail.com>
2014-05-18 17:48:03 +0900

    New: convertChunkToTree

--- src/ReadPhraseStructure.hs
+++ src/ReadPhraseStructure.hs
@@ -5,9 +5,12 @@ import Data.Char(isDigit)
 
 type ChunkID = Int
 type ModifieeID = Int
+-- 例: あたり あたり あたる 動詞 * 子音動詞ラ行 基本連用形
 data Morpheme = Morpheme String String String String String String String deriving (Show)
-data Chunk = Chunk ChunkID ModifieeID [Morpheme] deriving (Show)
+getPOS :: Morpheme -> String
+getPOS (Morpheme s0 s1 s2 s3 s4 s5 s6) = s3
 
+data Chunk = Chunk ChunkID ModifieeID [Morpheme] deriving (Show)
 type KyotoCorpusLine = String
 
 makeMorpheme :: KyotoCorpusLine -> Morpheme
@@ -17,7 +20,7 @@ makeMorpheme l = Morpheme (lst !! 0) (lst !! 1) (lst !! 2) (lst !! 3) (lst !! 4)
 makeChunkList :: [KyotoCorpusLine] -> [Chunk]
 makeChunkList [] = []
 makeChunkList (x:xs) | "*" `isPrefixOf` x = (Chunk chunkID modifieeID (map makeMorpheme mrphs)) : (makeChunkList others)
-                     | otherwise          = [] -- 起こりえない
+                     | otherwise          = undefined -- 起こりえない
   where wordLst = words x
         chunkID :: Int
         chunkID = read (wordLst !! 1) :: Int
@@ -26,10 +29,34 @@ makeChunkList (x:xs) | "*" `isPrefixOf` x = (Chunk chunkID modifieeID (map makeM
         mrphs  = takeWhile (\l -> not $ "*" `isPrefixOf` l) xs
         others = dropWhile (\l -> not $ "*" `isPrefixOf` l) xs
 
+
+data Tree a = Node a | Leaf (Tree a) (Tree a) deriving (Show)
+data ChunkTree = ChunkTree ChunkID ModifieeID (Tree Morpheme) deriving (Show)
+
+convertChunkToTree :: Chunk -> ChunkTree
+convertChunkToTree (Chunk chunkID modifieeID []) = undefined -- 起こりえない
+convertChunkToTree (Chunk chunkID modifieeID mrphs@(x:xs))
+  | any (\mrph -> getPOS mrph == "動詞") mrphs = ChunkTree chunkID modifieeID morphemeTree
+  | otherwise = ChunkTree chunkID modifieeID ansTree
+    where morphemeTree = foldl' (\ans m -> Leaf ans (Node m)) (Node x) xs
+
+          pre         = takeWhile (\mrph -> getPOS mrph /= "特殊") mrphs
+          pre_tail    = dropWhile (\mrph -> getPOS mrph /= "特殊") mrphs
+          nounPhrases = takeWhile (\mrph -> getPOS mrph /= "助詞" && getPOS mrph /= "特殊") pre_tail
+          suf         = dropWhile (\mrph -> getPOS mrph /= "助詞" && getPOS mrph /= "特殊") pre_tail
+
+          npTreeList = if (null nounPhrases) then [] else [foldr (\np ans -> Leaf (Node np) ans) (Node (last nounPhrases)) (init nounPhrases)]
+          ansTree = concatTreeListL $ (map Node pre) ++ npTreeList ++ (map Node suf)
+
+-- 木のリストをfold-leftでたたむ
+concatTreeListL :: [Tree a] -> Tree a
+concatTreeListL [] = undefined -- 想定してない
+concatTreeListL (x:xs) = foldl' (\ans tr -> Leaf ans tr) x xs
+
 main = do
   f' <- splitOn "EOS\n" <$> getContents
   let f = map lines f'
-      s1 = f !! 0
-      chunkTrees = map makeChunkList f
-      chunk = chunkTrees !! 0
-  mapM_ print chunk
+      stringChunkList = map makeChunkList f
+      chunkList = stringChunkList !! 0
+      tree = (map convertChunkToTree chunkList)
+  mapM_ print tree
